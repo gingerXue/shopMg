905561bef55685697df23560a6a116957da4851d
MXjz <597145896@qq.com>
2020-03-17 23:18:57 +0800

    项目搭建

---
+++ src/components/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-content">
+    登录{{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'hello'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .login-content {
+    background-image: url("../assets/bg.png");
+    background-size: 100% 100%;
+    height: 100%;
+  }
+</style>
